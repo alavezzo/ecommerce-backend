b15ac81bcecb54553743f84b61987537509006ba
bowdenshot <anthonyelavezzo@gmail.com>
2021-09-11 03:10:37 -0500

    files created, db created and seeded. Sequelized connected and initialized. Models created. GET Routes complete and tested in insomnia.

---
+++ models/index.js
@@ -0,0 +1,54 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+})
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  
+  foreignKey: 'product_id',
+  onDelete: 'Set Null'
+})
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  
+  foreignKey: 'tag_id',
+  onDelete: 'SET NULL'
+})
+
+ProductTag.belongsTo(Product, {
+  foreignKey: 'product_id',
+  onDelete: 'SET NULL'
+})
+
+ProductTag.belongsTo(Tag, {
+  foreignKey: 'tag_id',
+  onDelete: 'SET NULL'
+})
+
+Product.hasMany(ProductTag, {
+  foreignKey: 'product_id'
+})
+
+Tag.hasMany(ProductTag, {
+  foreignKey: 'tag_id'
+})
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
